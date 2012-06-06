119c307424468e2b83b4fffeb610546ec82fed46
stef <stefan.marsiske@gmail.com>
2012-06-06 02:47:27 +0200

    [enh] utf8 support in csv2json

--- scraptils/tools/csvtojson.py
+++ scraptils/tools/csvtojson.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
-import csv, json, sys
+import csv, json, sys, chardet
 
-reader = csv.DictReader( sys.stdin)
-headers = csv.reader( sys.stdin).next()
-reader.fieldnames=headers
+def UnicodeDictReader(utf8_data, **kwargs):
+    csv_reader = csv.DictReader(utf8_data, **kwargs)
+    for row in csv_reader:
+        try:
+            yield dict([(key, unicode(value, chardet.detect(value)['encoding'] or "ascii")) for key, value in row.iteritems()])
+        except UnicodeDecodeError:
+            yield dict([(key, unicode(value, "latin2")) for key, value in row.iteritems()])
+
+headers = csv.reader(sys.stdin, delimiter=';').next()
+reader = UnicodeDictReader(sys.stdin,fieldnames=headers, delimiter=';')
 for row in reader:
-    print json.dumps(row).replace('\n','')
+    print json.dumps(row).replace('\n','').encode('utf8')
