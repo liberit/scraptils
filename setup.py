6b406a3a1c3701ae2deebcbf3e8779e97bee8def
stef <stefan.marsiske@gmail.com>
2013-01-25 23:34:24 +0100

    [fix] trial&error

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "scraptils",
-    version = "0.2.0",
+    version = "0.2.1",
     author = "Stefan Marsiske",
     author_email = "s@ctrlc.hu",
     description = ("Scraping and datamangling Utilities"),
